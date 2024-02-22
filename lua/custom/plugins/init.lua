4353861cfe4db118e620968481a5f1b966fd52d0
Makoto Isechi <isechimakoto@gmail.com>
2024-02-22 12:13:35 +0900

    add markdown previewer

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -75,4 +75,10 @@ return {
       "vim-denops/denops.vim",
     }
   },
+  {
+    "iamcco/markdown-preview.nvim",
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    ft = { "markdown" },
+    build = function() vim.fn["mkdp#util#install"]() end,
+  },
 }
