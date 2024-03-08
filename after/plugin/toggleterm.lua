f904c9440a78904bb33416167a0b610c41f648f2
Makoto Isechi <isechimakoto@gmail.com>
2024-03-08 20:38:23 +0900

    disable lazygit

--- after/plugin/toggleterm.lua
+++ after/plugin/toggleterm.lua
@@ -1,11 +1,11 @@
-local Terminal = require('toggleterm.terminal').Terminal
-local lazygit = Terminal:new({ cmd = "lazygit", hidden = true })
-
-function _lazygit_toggle()
-    lazygit:toggle()
-end
-
-vim.api.nvim_set_keymap("n", "<leader>gg", "<cmd>lua _lazygit_toggle()<CR>", { noremap = true, silent = true })
+-- local Terminal = require('toggleterm.terminal').Terminal
+-- local lazygit = Terminal:new({ cmd = "lazygit", hidden = true })
+--
+-- function _lazygit_toggle()
+--     lazygit:toggle()
+-- end
+--
+-- vim.api.nvim_set_keymap("n", "<leader>gg", "<cmd>lua _lazygit_toggle()<CR>", { noremap = true, silent = true })
 
 function _set_terminal_keymaps()
   local opts = {buffer = 0}
