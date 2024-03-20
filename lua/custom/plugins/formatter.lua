4b74c35e75184fe4fd8679d5f8dea5f0b266fc38
Makoto Isechi <isechimakoto@gmail.com>
2024-03-21 00:12:27 +0900

    update formatter

--- lua/custom/plugins/formatter.lua
+++ lua/custom/plugins/formatter.lua
@@ -1,94 +1,25 @@
 return {
-  -- { 'mhartington/formatter.nvim' },
   {
     'stevearc/conform.nvim',
-    opts = {
-      notify_on_error = false,
-      -- format_on_save = {
-      --   timeout_ms = 500,
-      --   lsp_fallback = true,
-      -- },
-      formatters_by_ft = {
-        lua = { 'stylua' },
-        javascript = { { 'prettierd', 'prettier' } },
-        typescript = { { 'prettierd', 'prettier' } },
-      },
-    },
+    config = function()
+      local conform = require 'conform'
+
+      conform.setup {
+        -- notify_on_error = false,
+        -- format_on_save = {
+        --   timeout_ms = 500,
+        --   lsp_fallback = true,
+        -- },
+        formatters_by_ft = {
+          lua = { 'stylua' },
+          javascript = { { 'prettierd', 'prettier' } },
+          typescript = { { 'prettierd', 'prettier' } },
+          php = { 'php_cs_fixer' },
+        },
+      }
+
+      vim.keymap.set({ 'n', 'v' }, '<leader>lf', conform.format, { desc = 'Format' })
+    end,
   },
 }
 
--- -- Utilities for creating configurations
--- local util = require "formatter.util"
--- local prettier = require "formatter.defaults.prettier"
---
--- -- Provides the Format, FormatWrite, FormatLock, and FormatWriteLock commands
--- require("formatter").setup {
---   -- Enable or disable logging
---   logging = true,
---   -- Set the log level
---   log_level = vim.log.levels.WARN,
---   try_node_modules = true,
---   -- All formatter configurations are opt-in
---   filetype = {
---     -- Formatter configurations for filetype "lua" go here
---     -- and will be executed in order
---     lua = {
---       -- "formatter.filetypes.lua" defines default configurations for the
---       -- "lua" filetype
---       require("formatter.filetypes.lua").stylua,
---
---       -- You can also define your own configuration
---       function()
---         -- Supports conditional formatting
---         if util.get_current_buffer_file_name() == "special.lua" then
---           return nil
---         end
---
---         -- Full specification of configurations is down below and in Vim help
---         -- files
---         return {
---           exe = "stylua",
---           args = {
---             "--search-parent-directories",
---             "--stdin-filepath",
---             util.escape_path(util.get_current_buffer_file_path()),
---             "--",
---             "-",
---           },
---           stdin = true,
---         }
---       end
---     },
---
---     rust = {
---       require "formatter.filetypes.rust".rustfmt,
---     },
---
---     javascript = {
---       require "formatter.filetypes.javascript".prettier,
---     },
---
---     javascriptreact = {
---       require "formatter.filetypes.javascriptreact".prettier,
---     },
---
---     typescript = {
---       require "formatter.filetypes.typescript".prettier,
---     },
---
---     typescriptreact = {
---       require "formatter.filetypes.typescriptreact".prettier,
---     },
---
---     -- Use the special "*" filetype for defining formatter configurations on
---     -- any filetype
---     ["*"] = {
---       -- "formatter.filetypes.any" defines default configurations for any
---       -- filetype
---       require("formatter.filetypes.any").remove_trailing_whitespace,
---     }
---   }
--- }
---
--- vim.keymap.set('n', '<leader>lf', '<cmd>Format<cr>')
--- vim.keymap.set('n', '<leader>lF', '<cmd>FormatWrite<cr>')
