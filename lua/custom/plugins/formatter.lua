a70b41c79832fa6cc166294ae4f33b60ca6b91a1
Makoto Isechi <isechimakoto@gmail.com>
2024-04-08 09:05:16 +0900

    enable format on save, and formatting for several types of file(e.g. graphql, json)

--- lua/custom/plugins/formatter.lua
+++ lua/custom/plugins/formatter.lua
@@ -3,18 +3,20 @@ return {
     'stevearc/conform.nvim',
     config = function()
       local conform = require 'conform'
-
       conform.setup {
-        -- notify_on_error = false,
-        -- format_on_save = {
-        --   timeout_ms = 500,
-        --   lsp_fallback = true,
-        -- },
+        format_on_save = {
+          timeout_ms = 500,
+          lsp_fallback = true,
+        },
         formatters_by_ft = {
           lua = { 'stylua' },
           javascript = { { 'prettierd', 'prettier' } },
           typescript = { { 'prettierd', 'prettier' } },
+          javascriptreact = { { 'prettierd', 'prettier' } },
+          typescriptreact = { { 'prettierd', 'prettier' } },
           php = { 'php_cs_fixer' },
+          graphql = { { 'prettierd', 'prettier' } },
+          json = { { 'prettierd', 'prettier' } },
         },
       }
 
@@ -22,4 +24,3 @@ return {
     end,
   },
 }
-
