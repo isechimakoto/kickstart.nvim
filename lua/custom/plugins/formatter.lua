18503e476994a1e3e3b6d6b0d0c7d84258fd1d81
Makoto Isechi <isechimakoto@gmail.com>
2024-09-10 22:46:58 +0900

    update formatter settings

--- lua/custom/plugins/formatter.lua
+++ lua/custom/plugins/formatter.lua
@@ -1,29 +1,40 @@
 return {
   {
     'stevearc/conform.nvim',
-    config = function()
-      local conform = require 'conform'
-      local util = require 'conform.util'
-      conform.setup {
-        -- format_on_save = {
-        --   timeout_ms = 500,
-        --   lsp_fallback = true,
-        -- },
-        formatters_by_ft = {
-          lua = { 'stylua' },
-          css = { { 'prettierd', 'prettier' } },
-          scss = { { 'prettierd', 'prettier' } },
-          javascript = { { 'prettierd', 'prettier' } },
-          typescript = { { 'prettierd', 'prettier' } },
-          javascriptreact = { { 'prettierd', 'prettier' } },
-          typescriptreact = { { 'prettierd', 'prettier' } },
-          php = { 'php_cs_fixer' },
-          graphql = { { 'prettierd', 'prettier' } },
-          json = { { 'prettierd', 'prettier' } },
-        },
-      }
-
-      vim.keymap.set({ 'n', 'v' }, '<leader>lf', conform.format, { desc = 'Format' })
-    end,
+    event = { 'BufWritePre' },
+    cmd = { 'ConformInfo' },
+    keys = {
+      {
+        '<leader>lf',
+        function()
+          require('conform').format { async = true, lsp_fallback = true }
+        end,
+        mode = '',
+        desc = '[F]ormat buffer',
+      },
+    },
+    opts = {
+      notify_on_error = false,
+      -- format_on_save = function(bufnr)
+      --   local disable_filetypes = { c = true, cpp = true }
+      --   return {
+      --     timeout_ms = 500,
+      --     lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
+      --   }
+      -- end,
+      formatters_by_ft = {
+        lua = { 'stylua' },
+        -- You can use 'stop_after_first' to run the first available formatter from the list
+        css = { { 'prettierd', 'prettier', stop_after_first = true } },
+        scss = { { 'prettierd', 'prettier', stop_after_first = true } },
+        javascript = { { 'prettierd', 'prettier', stop_after_first = true } },
+        typescript = { { 'prettierd', 'prettier', stop_after_first = true } },
+        javascriptreact = { { 'prettierd', 'prettier', stop_after_first = true } },
+        typescriptreact = { { 'prettierd', 'prettier', stop_after_first = true } },
+        php = { 'pint', 'php_cs_fixer', stop_after_first = true },
+        graphql = { { 'prettierd', 'prettier', stop_after_first = true } },
+        json = { { 'prettierd', 'prettier', stop_after_first = true } },
+      },
+    },
   },
 }
