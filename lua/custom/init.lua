750a1b5e7def27c13a6c8384563f27c13b5c6344
Makoto Isechi <isechimakoto@gmail.com>
2025-01-31 15:42:51 +0900

    update settings

--- lua/custom/init.lua
+++ lua/custom/init.lua
@@ -1,3 +1,6 @@
-require("custom/misechi/set")
-require("custom/misechi/remap")
-require("custom/misechi/custom")
+require 'custom/misechi/set'
+require 'custom/misechi/remap'
+require 'custom/misechi/custom'
+
+-- https://github.com/neovim/neovim/issues/31675
+vim.hl = vim.highlight
--- lua/custom/plugins/treesitter.lua
+++ lua/custom/plugins/treesitter.lua
@@ -73,24 +73,29 @@ return {
             },
           },
         },
+
+        query_linter = {
+          enable = true,
+          use_virtual_text = true,
+          lint_events = { 'BufWrite', 'CursorHold' },
+        },
       }
 
-      local parser_config = require "nvim-treesitter.parsers".get_parser_configs()
+      local parser_config = require('nvim-treesitter.parsers').get_parser_configs()
       parser_config.blade = {
         install_info = {
-          url = "https://github.com/EmranMR/tree-sitter-blade",
-          files = {"src/parser.c"},
-          branch = "main",
+          url = 'https://github.com/EmranMR/tree-sitter-blade',
+          files = { 'src/parser.c' },
+          branch = 'main',
         },
-        filetype = "blade"
+        filetype = 'blade',
       }
-      vim.filetype.add({
+      vim.filetype.add {
         pattern = {
           ['.*%.blade%.php'] = 'blade',
         },
-      })
+      }
     end,
   },
-  { 'nvim-treesitter/playground' },
   { 'nvim-treesitter/nvim-treesitter-context' },
 }
