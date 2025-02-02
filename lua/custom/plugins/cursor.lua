923bb39e721cb567c3d04e9dc00bd09c85ebc50b
Makoto Isechi <isechimakoto@gmail.com>
2025-02-02 15:59:30 +0900

    update cursor, noice setting

--- lua/custom/plugins/cursor.lua
+++ lua/custom/plugins/cursor.lua
@@ -1,4 +1,7 @@
 return {
-  "sphamba/smear-cursor.nvim",
-  opts = {},
+  'sphamba/smear-cursor.nvim',
+  opts = {
+    legacy_computing_symbols_support = true,
+    transparent_bg_fallback_color = '#303030',
+  },
 }
