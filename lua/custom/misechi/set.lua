ed5a30cd31b53bf4ab4e47ac26c509504ee5c4ca
Makoto Isechi <isechimakoto@gmail.com>
2025-02-25 15:18:45 +0900

    update rename setting

--- lua/custom/misechi/set.lua
+++ lua/custom/misechi/set.lua
@@ -7,16 +7,16 @@ vim.opt.smartindent = true
 vim.opt.wrap = false
 vim.opt.swapfile = false
 vim.opt.backup = false
-vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+vim.opt.undodir = os.getenv 'HOME' .. '/.vim/undodir'
 vim.opt.undofile = true
 vim.opt.hlsearch = true
 vim.opt.incsearch = true
 vim.opt.termguicolors = true
 vim.opt.scrolloff = 50
-vim.opt.signcolumn = "yes"
-vim.opt.isfname:append("@-@")
+vim.opt.signcolumn = 'yes'
+vim.opt.isfname:append '@-@'
 vim.opt.list = true
-vim.opt.listchars = "tab:» ,trail:-"
+vim.opt.listchars = 'tab:» ,trail:-'
 vim.g.netrw_banner = 0
 vim.o.mouse = ''
 vim.opt.cursorline = false
@@ -24,9 +24,9 @@ vim.opt.winblend = 0
 vim.opt.pumblend = 0
 
 local has = vim.fn.has
-local is_mac = has "mac"
-local is_win = has "win32"
-local is_linux = has "linux"
+local is_mac = has 'mac'
+local is_win = has 'win32'
+local is_linux = has 'linux'
 
 if is_mac == 1 then
   vim.opt.clipboard:append { 'unnamedplus' }
@@ -38,14 +38,14 @@ end
 
 if is_linux == 1 then
   vim.g.clipboard = {
-    name = "xsel_override",
+    name = 'xsel_override',
     copy = {
-      ["+"] = "xsel --input --clipboard",
-      ["*"] = "xsel --input --primary",
+      ['+'] = 'xsel --input --clipboard',
+      ['*'] = 'xsel --input --primary',
     },
     paste = {
-      ["+"] = "xsel --output --clipboard",
-      ["*"] = "xsel --output --primary",
+      ['+'] = 'xsel --output --clipboard',
+      ['*'] = 'xsel --output --primary',
     },
     cache_enabled = 1,
   }
